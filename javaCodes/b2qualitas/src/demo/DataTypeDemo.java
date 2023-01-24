cd2032662effe8554434372be8a678b42ecec867
ragaPriya224 <90038032+ragaPriya224@users.noreply.github.com>
2023-01-24 23:30:42 +0530

    Add files via upload

---
+++ javaCodes/b2qualitas/src/demo/DataTypeDemo.java
@@ -0,0 +1,21 @@
+package demo;
+
+public class DataTypeDemo {
+
+	public static void main(String[] args) {
+		int price = 10;
+		System.out.println(price); //10
+		System.out.println("price"); //price
+		price = 20;
+		System.out.println(price); //20
+		int Price = 40;
+		System.out.println(Price);//40
+		int iceCream;
+		iceCream = 50; //shouldnot redeclare the variable
+		System.out.println(iceCream);
+		int iceCream = 70;
+		
+		
+	}
+
+}
