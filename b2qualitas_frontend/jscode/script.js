fd7b12447ed46c85760973b0a1c7a20ec50722c5
ragaPriya224 <90038032+ragaPriya224@users.noreply.github.com>
2023-02-13 21:48:18 +0530

    js code added

---
+++ b2qualitas_frontend/jscode/script.js
@@ -0,0 +1,52 @@
+
+// alert
+// x=3
+//   if(x<0)
+//   {
+//   alert ("Negative");
+//   }
+//   else
+//   {
+//   alert ("Positive");
+//   }
+
+
+// confirm
+
+// c=confirm("Do You Want to Continue")
+//   if(c)
+//   {
+//   alert ("Welcome")
+//   }
+//   else
+//   {
+//   alert ("Thank You")
+//   }
+
+//prompt
+// p=prompt("Enter the name", "aaa")
+// if(p=="spidy"){
+// alert("Welcome")
+// }else{
+// alert("we don't know you ")
+// }
+
+function buttonFunctionality() {
+// alert("button is clicked");
+
+var result = document.getElementById("id_1").value;
+document.getElementById("id_2").value = result;
+// alert(result);
+};
+
+
+function myFunction(){
+    alert(" health drink is changed");
+    var result = document.getElementById("drinks").value;
+    document.getElementById("id_3").value = result;
+}
+
+//ondblclick
+//onmouseover
+//onresize
+//onfocus
