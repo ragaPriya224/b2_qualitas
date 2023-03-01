30b9badd512ade9518a19dd43bbfb1ed65b7cd9d
ragaPriya224 <90038032+ragaPriya224@users.noreply.github.com>
2023-03-01 21:05:45 +0530

    Add files via upload

--- angular/angularDemo/my-dream-app/src/app/test/test.component.ts
+++ angular/angularDemo/my-dream-app/src/app/test/test.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  selector: 'app-test', //tag
+  templateUrl: './test.component.html', //html
+  styleUrls: ['./test.component.css'] //css
 })
 export class TestComponent {
 
+  //interpolation - bind  data of class to template
+
+  public name = "code";
+
+  greetUser(){
+    return this.name + "welcome";
+  }
+
+  public successClass = "text-success";
+
+  onClick(){
+    console.log("hi button is clicked");
+  }
+
+  public display = false;
 }
